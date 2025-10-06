46e024ea5442c6a5ca24e49f1fb46e1802a13f2f
Hector Melendez <hector.melendez129@gmail.com>
2025-10-05 20:41:54 -0400

    052040ROCT25 added style, test cases in comments, and error handling for name and description

--- js/main.js
+++ js/main.js
@@ -1,50 +1,23 @@
-//The user will enter a date. 
-// Use that date to get the NASA picture of the day from that date! https://api.nasa.gov/
-
-// api key for nasa:   u6GNasxyDF98i7Lsfld9MVtlVsXorWBasAUkvwjr
-/*
-
-create const for button doc.qs
-activate button with .addevli(click, func)
-
-grab text value, convert to yyyy-mm-dd
-
-create function called getNasaDailyPic
-* Create the following inside of the function
-** create the const url with backtick, set interpolation for date
-** create the fetch call
-***write data.title inside of the h2 and img-alt
-***set img-src to data.url
-***set h3 to data.explanation
-*** grab date value, convert to yyyy-mm-dd
-*/
-
-
+//Activate button, add event listener
 const getPicbtn = document.querySelector('button')
 getPicbtn.addEventListener('click', getNasaDailyPic)
 
 function getNasaDailyPic() {
     //grab date input
     const userDate = document.querySelector('input').value
-    //insert date into url
+    // Insert date into url
     const url = `https://api.nasa.gov/planetary/apod?api_key=u6GNasxyDF98i7Lsfld9MVtlVsXorWBasAUkvwjr&date=${userDate}`
 
     fetch(url)
         .then(res => res.json()) //parse response into aa json
         .then(data => {
-            //logs json, title, url, explanations is description
-            console.log(data)
-            console.log(data.title)
-            console.log(data.url)
-            console.log(data.explanation)
+            //set h2 to data.title or default message           
+            document.querySelector('h2').innerText = data.title || "Sorry, please choose another date"
 
-            //set h2 to data.title            
-            document.querySelector('h2').innerText = data.title
+            //replace h3 with data.explanation or default message
+            document.querySelector('h3').innerText = data.explanation || ""
 
-            //replace h3 with data.explanation
-            document.querySelector('h3').innerText = data.explanation
-
-            //pull image
+            // Pull image, test on 13 Septemeber 2025
             if (data.url.includes("image")) {
                 document.querySelector('#displayImage').innerHTML =
                     `<img width="560" height="315" 
@@ -55,7 +28,7 @@ function getNasaDailyPic() {
                 document.querySelector('#youtubePlayer').innerHTML = "";
                 document.querySelector('#mp4Player').innerHTML = "";
 
-                //display youtube Video
+                //display youtube Video, test on 14 Septemeber 2025
             } else if (data.url.includes('youtube')) {
                 document.querySelector('#youtubePlayer').innerHTML =
                     `<iframe width="560" height="315" 
@@ -68,7 +41,8 @@ function getNasaDailyPic() {
                 document.querySelector('#displayImage').innerHTML = "";
                 document.querySelector('#mp4Player').innerHTML = "";
 
-                //display mp4 player
+                //display mp4 player, Test on 15 Septemeber 2025 
+                // Outlyer that will not work
             } else if (data.media_type.includes("other")) {
 
                 document.querySelector('#mp4Player').innerHTML =
@@ -81,7 +55,7 @@ function getNasaDailyPic() {
                 document.querySelector('#displayImage').innerHTML = "";
 
             } else {
-                console.log('Not A Supported Media')
+                document.querySelector('#mp4Player').innerHTML = 'Sorry, photo not available';
             }
         })
         .catch(err => {
